f6af70ae475c6fe493098f3ba66e888cb609b566
Bradley Mcfarland <bradley.mcfarland1@outlook.com>
2023-09-06 10:45:45 -0700

    Refactor using, Dictionary and List to try and make it cleaner.

--- DSA/GroceryStore/Program.cs
+++ DSA/GroceryStore/Program.cs
@@ -7,7 +7,7 @@ namespace GroceryStore
     {
         public static void Main(string[] args)
         {
-            /*
+            
             Dictionary<string, decimal> productAndPrice = new Dictionary<string, decimal>()
             {
                 {"apple", 1.99m },
@@ -15,69 +15,97 @@ namespace GroceryStore
                 {"watermelon", 3.99m },
                 {"peach", 1.29m }
             };
-            */
+            #region Initializing Variables
             decimal apple = 1.99m;
             decimal banana = 2.49m;
             decimal watermelon = 3.99m;
             decimal peach = 1.29m;
 
             decimal totalPrice = 0m;
-            int totalItems = 0;
+            int totalItems = 1;
             int totalApples = 0;
             int totalBananas = 0;
             int totalWatermelons = 0;
             int totalPeach = 0;
+            #endregion
 
-            Console.WriteLine("Welcome to the Grocery Store!");
-            Console.WriteLine("Enter the items you want to scan (e.g., 'apple', 'banana', 'watermelon', 'peach', or 'done' to finish):");
+            Console.WriteLine("Welcome to the Fresh Market!");
+            Console.WriteLine("We have:");
+            Console.WriteLine("- - - - - - - - - - - - - ");
+            Console.WriteLine("Apple     |       Banana");
+            Console.WriteLine("- - - - - - - - - - - - - ");
+            Console.WriteLine("Peach     |   Watermelon");
+            Console.WriteLine("- - - - - - - - - - - - - ");
+            Console.WriteLine("When finished enter 'done'");
+            Console.WriteLine();
+            Console.WriteLine("Enter the items you want:");
 
             List<string> itemList = new List<string>();
 
+
+            #region WhileLoop, IfStatements, Switch
             while (true)
             {
                 Console.Write("Enter item name: ");
                 string itemName = Console.ReadLine().ToLower();
+                Console.WriteLine("- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -");
+                Console.WriteLine($"You scanned, {itemName} now you have a total of {totalItems} items in your cart.");
+                Console.WriteLine("- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -");
+                Console.WriteLine("OK what else?");
 
                 if (itemName == "done")
                 {
-                    break; // Exit the loop when 'done' is entered.
+                    break; 
                 }
-                /*
+                
                 if(productAndPrice.ContainsKey(itemName))
                 {
-                    itemList += productAndPrice[]
+                    itemList.Add(itemName);
                     totalPrice += productAndPrice[itemName];
+                    totalItems++;
                 }
-                */
-                
-                switch (itemName)
+                else
                 {
-                    case "apple":
-                        totalPrice += apple;
-                        totalItems++;
-                        totalApples++;
-                        break;
-                    case "banana":
-                        totalPrice += banana;
-                        totalItems++;
-                        totalBananas++;
-                        break;
-                    case "watermelon":
-                        totalPrice += watermelon;
-                        totalItems++;
-                        totalWatermelons++;
-                        break;
-                     case "peach":
-                        totalPrice += peach;
-                        totalItems++;
-                        totalPeach++;
-                        break;
-                    default:
-                        Console.WriteLine("Item not recognized. Please try again.");
-                        break;
+                    Console.WriteLine("What is this? Enter again.");
                 }
-            }
+                
 
+
+                #region SwitchCase
+                /* 
+                 switch (itemName)
+                 {
+                     case "apple":
+                         totalPrice += apple;
+                         totalItems++;
+                         totalApples++;
+                         break;
+                     case "banana":
+                         totalPrice += banana;
+                         totalItems++;
+                         totalBananas++;
+                         break;
+                     case "watermelon":
+                         totalPrice += watermelon;
+                         totalItems++;
+                         totalWatermelons++;
+                         break;
+                      case "peach":
+                         totalPrice += peach;
+                         totalItems++;
+                         totalPeach++;
+                         break;
+                     default:
+                         Console.WriteLine("Item not recognized. Please try again.");
+                         break;
+                 }
+                */
+                #endregion
+            }
+            #endregion
+
+            #region StringBuilderOld
+            /*
             StringBuilder receipt = new StringBuilder();
             receipt.AppendLine("Receipt:");
             receipt.AppendLine("---------");
@@ -89,10 +117,26 @@ namespace GroceryStore
 
             receipt.AppendLine("---------");
             receipt.AppendLine("Total items purchased: " + totalItems.ToString("0") + " Total: $" + totalPrice.ToString("0.00"));
+            */
+            #endregion
+
+            #region StringBuilderNew
+
+            StringBuilder receipt = new StringBuilder();
+            receipt.AppendLine("- - - - - - - - - - - - - ");
+            receipt.Append(" ");
+
+            foreach(string item in itemList) 
+            {
+                receipt.Append($"\n {item}: {productAndPrice[item]:0.00} dollars");
+            }
+            receipt.AppendLine("\n- - - - - - - - - - - - - ");
+            receipt.AppendLine($"Total: ${totalPrice:0.00}");
+            #endregion
 
             Console.WriteLine(receipt.ToString());
 
-            Console.WriteLine("Thank you for shopping with us!");
+            Console.WriteLine("Thanks for shopping with us!");
         }
      }
 }
